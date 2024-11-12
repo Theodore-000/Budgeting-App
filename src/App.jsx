41f653f72b7fdbd680bfbd76c5b1f3daf3aa9804
Theodore-000 <teddytez000@gmail.com>
2024-11-12 18:30:18 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Template from './components/Template';
+
+const App = () => {
+  return ( 
+    <div className="flex items-center justify-center h-80 m-auto">
+      <Template/>
+    </div>
+   );
+}
+ 
+export default App;
+
