a4eac1e4fb14d9b85dbfbcd502ebe7fa3ea30c2b
Theodore-000 <teddytez000@gmail.com>
2024-11-12 18:50:16 +0300

    new package installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
-import Template from './components/Template';
-
 const App = () => {
-  return ( 
-    <div className="flex items-center justify-center h-80 m-auto">
-      <Template/>
-    </div>
-   );
-}
- 
-export default App;
+  return <div className="flex items-center justify-center h-80 m-auto"></div>;
+};
 
+export default App;
